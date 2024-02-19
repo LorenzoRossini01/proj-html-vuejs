1ace7d73dbd2b3dff600973a58985e84fb5d18af
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-19 12:06:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import axios from "axios";
+import { store } from "./store/index";
+
+export default {
+  data() {
+    return { store };
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import Alert from "bootstrap/js/dist/alert";
+
+createApp(App).mount("#app");
